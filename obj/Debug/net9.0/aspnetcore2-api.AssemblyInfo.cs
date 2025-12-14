7462fd85cfc7cdff73c4e055ba9c46ddbcf556d4
Milton Lima <milton.mlima@gmail.com>
2025-12-14 19:52:46 -0300

    Atualiza metadados da assembly e arquivos binários após a compilação

--- obj/Debug/net9.0/aspnetcore2-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/aspnetcore2-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6e915db08f10b41d5d822d3c8d24f91cd94fd9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+906422296fc6bb42bae40ac82831707555fb5e2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
