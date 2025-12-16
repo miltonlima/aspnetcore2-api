033c4601da1066eabed176345a2d604d5692b9d4
Milton Lima <milton.mlima@gmail.com>
2025-12-16 16:57:30 -0300

    Atualizar a mensagem de resposta do endpoint "/ping" e regenerar os arquivos relacionados.

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
 
 // Endpoint de teste simples que retorna uma string.
-app.MapGet("/ping", () => "Vindo da API!").WithName("Ping");
+app.MapGet("/ping", () => "Vindo da API!!").WithName("Ping");
 
 var summaries = new[]
 {
