20979d6e5820093c001fb8c3882c21fa337e9bc7
Milton Lima <milton.mlima@gmail.com>
2025-12-17 11:39:02 -0300

    Atualizar a versão informativa do assembly e regenerar arquivos relacionados

--- obj/Debug/net9.0/aspnetcore2-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/aspnetcore2-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12bdf2d9e7736810b7834f38bd381ff86dd15fa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb2d46fe6b270e0019586ae127d03904426ff4cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
