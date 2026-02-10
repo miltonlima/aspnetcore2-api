b6d8851f49008bb264eb0d32e72337e7447db218
Milton Lima <milton.mlima@gmail.com>
2026-02-10 20:36:12 -0300

    Atualizar arquivos binários e de cache após a modificação da versão informativa do assembly

--- obj/Debug/net9.0/aspnetcore2-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/aspnetcore2-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f39988517fe2807c090694f0c7e0eff9cd7915b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0af333bac7f6d652dbc49a44023e667cedb0b6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
