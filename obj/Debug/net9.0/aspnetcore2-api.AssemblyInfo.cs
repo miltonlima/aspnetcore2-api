179cabf0234e8473618c9431845fdbfafae009fe
Milton Lima <milton.mlima@gmail.com>
2026-02-19 16:38:02 -0300

    Atualizar string de conexão do Supabase para valores genéricos e ajustar informações de versão nos arquivos de configuração

--- obj/Debug/net9.0/aspnetcore2-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/aspnetcore2-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03a3b3432ff2cb6ce7e648fac688df133f3da95c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+222d07959c8a05f4bdeea2c868fa67c9977ede51")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
