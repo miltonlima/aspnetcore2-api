00abcfe1099c781d7e4d5106f72c94a2b4cb4348
Milton Lima <milton.mlima@gmail.com>
2026-04-06 00:15:00 -0300

    Atualizar binários e arquivos de configuração para refletir as mudanças recentes e alinhar versões de pacotes.

--- obj/Debug/net10.0/aspnetcore2-api.AssemblyInfo.cs
+++ obj/Debug/net10.0/aspnetcore2-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c805466824cf6c8784ed0b3cd8846dc69b2db99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+757f2b1ba7f9ba3b6bd0583407334f52b3461c9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
