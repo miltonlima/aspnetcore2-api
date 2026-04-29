d5dc81dcb638b2a36b3a8685a4907fa72be9495e
Milton Lima <milton.mlima@gmail.com>
2026-04-28 23:45:01 -0300

    Atualizar arquivos binários e de configuração após a compilação

--- obj/Debug/net8.0/aspnetcore2-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/aspnetcore2-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+878f11bf65614f4e5d460018061a55cd52c10295")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c303a0636ab9f5c87054955ce97b9151178fbd06")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetcore2-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
